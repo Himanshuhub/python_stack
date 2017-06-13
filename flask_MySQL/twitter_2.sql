73f98bdff5a6f9f833cb8ac4cec0ee31f436ffe4
Himanshu <himanshuguptams@gamil.com>
2017-06-13 12:58:06 -0700

    ERD, SQL

---
+++ flask_MySQL/twitter_2.sql
@@ -0,0 +1,50 @@
+select first_name as first, last_name as last 
+from users
+where id = 1 or 2;
+
+SELECT * 
+from users
+WHERE id = 1 OR 2
+ORDER BY birthday;
+
+SELECT * 
+FROM faves;
+
+SELECT * 
+FROM follows;
+
+SELECT * 
+FROM tweets;
+
+SELECT * 
+FROM users;
+
+SELECT first_name, last_name 
+FROM users
+WHERE first_name like 'k%' ;
+
+SELECT * 
+FROM users
+WHERE first_name like '%e' ;
+
+SELECT * 
+FROM users
+WHERE first_name like 'k%' ;
+
+SELECT * 
+FROM users
+ORDER BY birthday DESC;
+
+SELECT * 
+FROM USERS
+ORDER BY BIRTHDAY ASC;
+
+SELECT * 
+FROM USERS
+WHERE FIRST_NAME LIKE '%e'
+ORDER BY BIRTHDAY DESC;
+
+SELECT first_name
+FROM users 
+ORDER BY first_name;
+
---
+++ flask_MySQL/twitter_INSERT.sql
@@ -0,0 +1,5 @@
+SELECT * 
+FROM tweets;
+
+INSERT INTO tweets(tweet, user_id, created_at, updated_at)
+VALUES('hellooo', 1, NOW(), now());
