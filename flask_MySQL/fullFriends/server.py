5ed0488d2bf01b13de86bcda762133a066e2c898
Himanshu <himanshuguptams@gamil.com>
2017-06-15 13:31:02 -0700

    friends

--- flask_MySQL/fullFriends/server.py
+++ flask_MySQL/fullFriends/server.py
@@ -1,9 +1,23 @@
-from flask import Flask
-# import the Connector function
+from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
-# connect and store the connection in "mysql" note that you pass the database name to the function
-mysql = MySQLConnector(app, 'mydb')
-# an example of running a query
-print mysql.query_db("SELECT * FROM users")
+mysql = MySQLConnector(app,'friendsdb')
+
+@app.route('/')
+def index():
+    query = "SELECT  *, created_at as friendSince, YEAR ( created_at ) as yearJoined FROM friends"                           # define your query
+    friends = mysql.query_db(query)                           # run query with query_db()
+    return render_template('index.html', all_friends=friends) # pass data to our template
+
+@app.route('/friends', methods=['POST'])
+def create():
+    query = "INSERT INTO friends (first_name, last_name, age, created_at, updated_at) VALUES (:first_name, :last_name, :age, NOW(), NOW())"
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'age': request.form['age']
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+
 app.run(debug=True)
