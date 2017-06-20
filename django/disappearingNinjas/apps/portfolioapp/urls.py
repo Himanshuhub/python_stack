5e07d8f91f93cf860f92dbf57f4052b318be1906
Himanshu <himanshuguptams@gamil.com>
2017-06-20 12:01:33 -0700

    realportfolio

--- django/disappearingNinjas/apps/portfolioapp/urls.py
+++ django/disappearingNinjas/apps/portfolioapp/urls.py
@@ -3,8 +3,8 @@ from . import views           # This line is new!
 urlpatterns = [
 url(r'^$', views.index),     # This line has changed!
 url(r'^ninjas$', views.ninjas),
-url(r'^aboutme$', views.aboutme),
-url(r'^projects$', views.projects),
-url(r'^new_user$', views.create),
+# url(r'^aboutme$', views.aboutme),
+# url(r'^projects$', views.projects),
+# url(r'^new_user$', views.create),
 url(r'^ninjas/(?P<color>[a-z]+)$', views.show)
 ]
--- django/disappearingNinjas/apps/portfolioapp/views.py
+++ django/disappearingNinjas/apps/portfolioapp/views.py
@@ -1,64 +1,25 @@
 from django.shortcuts import render, HttpResponse, redirect
-# the index function is called when root is visited
-# def index(request):
-#   print "*"*50
-#   return render(request, 'portfolioapp/index.html')
 
 def index(request):
 	return render(request, 'portfolioapp/index.html')
 
-# /ninjas
 def ninjas(request):
 	print request.method
 	return render(request, 'portfolioapp/ninjas.html')
 
 def show(request, color):
 	if color == 'blue':
-		context = {
-		'color' : color
-		}
-		return render(request, 'portfolioapp/blue.html', context)
+		image = 'leonardo.jpg'
 	elif color == 'orange':
-		context = {
-		'color' : color
-		}
-		return render(request, 'portfolioapp/orange.html', context)
+		image = 'michelangelo.jpg'
 	elif color == 'red':
-		context = {
-		'color' : color
-		}
-		return render(request, 'portfolioapp/red.html', context)
+		image = 'raphael.jpg'
 	elif color == 'purple':
-		context = {
-		'color' : color
-		}
-		return render(request, 'portfolioapp/purple.html', context)
-
-
+		image = 'donatello.jpg'
 	else:
-		context = {
-		'color' : color
-		}
-		return render(request, 'portfolioapp/notapril.html', context)
-
-
-def aboutme(request):
-	print request.method
-	return render(request, 'portfolioapp/aboutme.html')
-
-def projects(request):
-	print request.method
-	return render(request, 'portfolioapp/projects.html')
-
-def create(request):
-	request.session['name'] = request.POST['first_name']
-
-	if request.method == "POST":
-		print "*"*50
-		print request.POST
-		print request.method
-		print "*"*50
-		return redirect("/")
-	else:
-		return redirect("/")
-		# return render(request, 'portfolioapp/projects.html')
+		image = 'notapril.jpg'
+		# return render(request, 'portfolioapp/notapril.html', context)
+	context = {
+		'image' : image
+	}
+	return render(request, 'portfolioapp/purple.html', context)
