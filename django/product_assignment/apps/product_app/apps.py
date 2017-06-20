2b42b0d5c43cd33b3a5703650ed704985c7270e0
Himanshu <himanshuguptams@gamil.com>
2017-06-20 14:27:10 -0700

    realportfolio

---
+++ django/product_assignment/apps/product_app/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ProductAppConfig(AppConfig):
+    name = 'product_app'
---
+++ django/product_assignment/apps/product_app/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=38)
+  description = models.CharField(max_length=500)
+  weight = models.IntegerField()
+  price = models.IntegerField()
+  cost = models.IntegerField()
+  category = models.CharField(max_length=500)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
